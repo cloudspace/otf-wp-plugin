ea61f2f69857cab4e87425fe036646444ce8d8f5
Josh Lindsey <josh@cloudspace.com>
2010-12-03 10:08:06 -0500

    Add GPL comment to php file, and Change Author URI to the correct link

--- otf.php
+++ otf.php
@@ -5,7 +5,23 @@ Plugin URI: http://onetruefan.com
 Description: Display the OneTrueFan bar on your blog.
 Version: 1.0
 Author: OneTrueFan
-Author URI: http://onetruefan.com
+Author URI: http://account.onetruefan.com/widget?wp-plugin
+*/
+
+/*  Copyright 2010 OneTrueFan  (email : info@onetruefan.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 add_action('wp_footer', 'add_otf_widget');
